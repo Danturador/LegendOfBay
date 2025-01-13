742dd9d567fb1742fa9a49d2037a3913e69ce50f
Danturador <luchenokdan@gmail.com>
2025-01-13 15:04:41 +0300

    Шлифовка карты
    Добавление мелких деталей, коллайдеров карте
    Добавлена логика яме с шипами (только перенос игрока)

---
+++ Assets/_ProjectFiles/FeaturesLevel/LevelElements/IneractiveObjects/Scripts/SpikePit.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class SpikePit : MonoBehaviour
+{
+    public Transform teleportDestination;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<PlayerController>() != null)
+        {
+            collision.transform.position = teleportDestination.position;
+            Debug.Log("Entered");
+        }
+    }
+}
