4a73fbcab4de03cc2fc53e3fe12cc4640e323763
Ivan <berezko2004@gmail.com>
2025-04-25 17:17:27 +0300

    Update GameData.cs

--- Assets/_ProjectFiles/SaveSystem/GameData/GameData.cs
+++ Assets/_ProjectFiles/SaveSystem/GameData/GameData.cs
@@ -12,16 +12,19 @@ namespace _ProjectFiles.SaveSystem
         public float PlayerHealth { get; private set; }
         public Vector3 Position { get; private set; }
         public SpawnersHolderData SpawnersHolderData { get; private set; }
-        
+        public Sprite Map { get; private set; }
+
         public GameData()
         {
             PlayerHealth = 100f;
             Position = Vector3.zero;
             SpawnersHolderData = new SpawnersHolderData();
+            Map = null;
         }
 
         public void SetPlayerHealth(float playerHealth) => PlayerHealth = playerHealth;
         public void SetPosition(Vector3 position) => Position = position;
         public void SetSpawners(SpawnersHolderData data) => SpawnersHolderData = data;
+        public void SetMap(Sprite map) => Map = map;
     }
 }
