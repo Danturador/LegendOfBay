6dac6f95b9f7a4afb2191383c9fdcec6ed4cfb97
Keige125 <NiskoromniiNL@outlook.com>
2025-04-27 10:38:50 +0300

    Add enemy ( monster ) hp bar

--- Assets/_ProjectFiles/FutureCharacter/Scripts/Combat System/MonsterHealth.cs
+++ Assets/_ProjectFiles/FutureCharacter/Scripts/Combat System/MonsterHealth.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,13 +6,30 @@ using UnityEngine.UIElements;
 
 public class MonsterHealth : HealthManager, IDamageable
 {
+    [SerializeField] private GameObject _hpBarGameobject;
+    public event Action<float> HealthChanged;
+
+    private void Start()
+    {
+        _hpBarGameobject = GetComponentInChildren<Canvas>()?.gameObject;
+        if(_hpBarGameobject != null)
+        {
+            _hpBarGameobject.SetActive(false);
+        } 
+    }
     public override void TakeDamage(int damage)
     {
         base.TakeDamage(damage);
+        float curentHealthPercantage = (float)currentHealth / maxHealth;
+        HealthChanged?.Invoke(curentHealthPercantage);
     }
     protected override void HealthUpdate()
     {
         base.HealthUpdate();
+        if(_hpBarGameobject != null && _hpBarGameobject.activeSelf == false)
+        {
+            _hpBarGameobject.SetActive(true);
+        }
     }
 
     protected override void Die()
