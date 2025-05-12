30cb01ae00216d6ccf9155d9e61d1438be9e7bcc
Danturador <luchenokdan@gmail.com>
2025-05-12 12:24:40 +0300

    фикс облаков, скорость хуньд, иконки ключей,

--- Assets/_ProjectFiles/FeaturesLevel/LevelElements/IneractiveObjects/Scripts/KeysAndDoors/Inventory.cs
+++ Assets/_ProjectFiles/FeaturesLevel/LevelElements/IneractiveObjects/Scripts/KeysAndDoors/Inventory.cs
@@ -2,11 +2,14 @@ using System.Collections.Generic;
 using System.Linq;
 using _ProjectFiles.SaveSystem;
 using UnityEngine;
+using UnityEngine.UI;
 using Zenject;
 
 public class Inventory : MonoBehaviour
 {
 	[Inject] private SaveSystemController _saveSystem;
+	[SerializeField] private Image greenKey;
+	[SerializeField] private Image redKey;
 
 	private HashSet<string> _keys = new();
 	private bool _initialized;
@@ -14,6 +17,7 @@ public class Inventory : MonoBehaviour
 	private void Awake()
 	{
 		_keys = _saveSystem.gameData.Inventory.ToHashSet();
+		ShowKeyIcons();
 	}
 
 	public bool AddKey(string id)
@@ -22,6 +26,7 @@ public class Inventory : MonoBehaviour
 			return false;
 		
 		_saveSystem.UpdateInventory(_keys.ToList());
+		ShowKeyIcons();
 		return true;
 	}
 
@@ -31,11 +36,22 @@ public class Inventory : MonoBehaviour
 			return false;
 		
 		_saveSystem.UpdateInventory(_keys.ToList());
+		ShowKeyIcons();
 		return true;
 	}
 
 	public string GetKeyId(string keyID)
 	{
 		return _keys.TryGetValue(keyID, out string value) ? value : null;
+	}
+	public bool HasKey(string keyID)
+	{
+		return _keys.Contains(keyID);
+	}
+
+	private void ShowKeyIcons()
+	{
+		greenKey.gameObject.SetActive(HasKey("0"));
+		redKey.gameObject.SetActive(HasKey("1"));
 	}
 }
